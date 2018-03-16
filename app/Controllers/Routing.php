e5a7828be9e03396f910212e92fb7bcd7c8bd1ff
Visman <visman2008@googlemail.com>
2018-03-16 20:01:06 +0700

    2018-03-16

--- app/Controllers/Routing.php
+++ app/Controllers/Routing.php
@@ -80,7 +80,10 @@ class Routing
                 $r->add('GET',  '/userlist[/{sort:username|registered|num_posts}/{dir:ASC|DESC}/{group:\-1|[1-9]\d*}/{name}][/{page:[1-9]\d*}]', 'Userlist:view', 'Userlist');
                 $r->add('POST', '/userlist', 'Userlist:view');
                 // юзеры
-                $r->add('GET', '/user/{id:[1-9]\d*}/{name}', 'Profile:view', 'User'); //????
+                $r->add('GET', '/user/{id:[2-9]|[1-9]\d+}/{name}', 'Profile:view', 'User'); //????
+            } elseif (! $user->isGuest) {
+                // только свой профиль
+                $r->add('GET', '/user/{id:' . $user->id . '}/{name}', 'Profile:view', 'User');
             }
             // пометка разделов прочитанными
             if (! $user->isGuest) {
