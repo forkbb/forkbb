b79618c6b93577581af83be4e8bc8b2e6ead1cee
Visman <visman2008@googlemail.com>
2018-09-16 17:26:47 +0700

    * Change the "Go back" button in Message

--- app/templates/message.forkbb.php
+++ app/templates/message.forkbb.php
@@ -3,6 +3,6 @@
       <h2>{!! __('Info') !!}</h2>
       <p>{!! $p->message !!}</p>
 @if ($p->back)
-      <p><a href="javascript: history.go(-1)">{!! __('Go back') !!}</a></p>
+      <p><a href="{!! $p->fRootLink !!}" onclick="window.history.back(); return false;">{!! __('Go back') !!}</a></p>
 @endif
     </section>
