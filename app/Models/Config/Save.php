ed4ec053fe5d74318d696536cc0fcf30a0c7d0bb
Visman <visman2008@googlemail.com>
2020-08-17 18:30:00 +0700

    Update save method for Config model
    Unset properties of the model will be removed from the database when it is written.

--- app/Models/Config/Save.php
+++ app/Models/Config/Save.php
@@ -22,31 +22,41 @@ class Save extends Method
 
         $values = $this->model->getAttrs();
         foreach ($modified as $name) {
-            $vars = [
-                ':value' => $values[$name],
-                ':name'  => $name
-            ];
-            //????
-            //????
-            $query = 'UPDATE ::config
-                SET conf_value=?s:value
-                WHERE conf_name=?s:name';
+            if (\array_key_exists($name, $values)) {
+                $vars = [
+                    ':value' => $values[$name],
+                    ':name'  => $name
+                ];
+                //????
+                //????
+                $query = 'UPDATE ::config
+                    SET conf_value=?s:value
+                    WHERE conf_name=?s:name';
 
-            $count = $this->c->DB->exec($query, $vars);
-            //????
-            //????
-            if (0 === $count) {
+                $count = $this->c->DB->exec($query, $vars);
                 //????
                 //????
-                $query = 'INSERT INTO ::config (conf_name, conf_value)
-                    SELECT ?s:name, ?s:value
-                    FROM ::groups
-                    WHERE NOT EXISTS (
-                        SELECT 1
-                        FROM ::config
-                        WHERE conf_name=?s:name
-                    )
-                    LIMIT 1';
+                if (0 === $count) {
+                    //????
+                    //????
+                    $query = 'INSERT INTO ::config (conf_name, conf_value)
+                        SELECT ?s:name, ?s:value
+                        FROM ::groups
+                        WHERE NOT EXISTS (
+                            SELECT 1
+                            FROM ::config
+                            WHERE conf_name=?s:name
+                        )
+                        LIMIT 1';
+
+                    $this->c->DB->exec($query, $vars);
+                }
+            } else {
+                $vars = [
+                    ':name'  => $name
+                ];
+                $query = 'DELETE FROM ::config
+                    WHERE conf_name=?s:name';
 
                 $this->c->DB->exec($query, $vars);
             }
