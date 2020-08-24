2a2f6ded08c487581137b9daea49ac96433bdb22
Visman <visman2008@googlemail.com>
2020-08-24 18:32:11 +0700

    Update feeds 3

--- app/templates/feed_atom.forkbb.php
+++ app/templates/feed_atom.forkbb.php
@@ -1,25 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-  <title type="text">{!! $p->e($p->feed['title']) !!}</title>
+  <title type="text">{{ $p->feed['title'] }}</title>
   <link rel="self" type="application/atom+xml" href="{!! $p->feed['id'] !!}" />
   <link rel="alternate" type="text/html" href="{!! $p->feed['link'] !!}" />
-  <updated>{!! $p->e(\gmdate('c', $p->feed['updated'])) !!}</updated>
+  <updated>{{ \gmdate('c', $p->feed['updated']) }}</updated>
   <generator>ForkBB</generator>
-  <id>{!! $p->e($p->feed['id']) !!}</id>
+  <id>{{ $p->feed['id'] }}</id>
 @foreach($p->feed['items'] as $item)
   <entry>
-    <title>{!! $p->e($item['title']) !!}</title>
+    <title>{{ $item['title'] }}</title>
     <link rel="alternate" type="text/html" href="{!! $item['link'] !!}" />
-    <id>{!! $p->e($item['id']) !!}</id>
-    <updated>{!! $p->e(\gmdate('c', $item['updated'])) !!}</updated>
-    <published>{!! $p->e(\gmdate('c', $item['published'])) !!}</published>
-		<author>
-			<name>{!! $p->e($item['author']) !!}</name>
+    <id>{{ $item['id'] }}</id>
+    <updated>{{ \gmdate('c', $item['updated']) }}</updated>
+    <published>{{ \gmdate('c', $item['published']) }}</published>
+    <author>
+      <name>{{ $item['author'] }}</name>
     @if ($item['isEmail'])
-			<email>{!! $p->e($item['email']) !!}</email>
+      <email>{{ $item['email'] }}</email>
     @endif
-		</author>
-    <content type="html">{!! $p->e($item['content']) !!}</content>
+    </author>
+    <content type="html">{{ $item['content'] }}</content>
   </entry>
 @endforeach
 </feed>
--- app/templates/feed_rss.forkbb.php
+++ app/templates/feed_rss.forkbb.php
@@ -2,19 +2,19 @@
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <atom:link href="{!! $p->feed['id'] !!}" rel="self" type="application/rss+xml" />
-    <title>{!! $p->e($p->feed['title']) !!}</title>
-    <link>{!! $p->e($p->feed['link']) !!}</link>
-    <description>{!! $p->e($p->feed['description']) !!}</description>
-    <pubDate>{!! $p->e(\gmdate('r', $p->feed['updated'])) !!}</pubDate>
+    <title>{{ $p->feed['title'] }}</title>
+    <link>{{ $p->feed['link'] }}</link>
+    <description>{{ $p->feed['description'] }}</description>
+    <pubDate>{{ \gmdate('r', $p->feed['updated']) }}</pubDate>
     <generator>ForkBB</generator>
 @foreach($p->feed['items'] as $item)
     <item>
-      <title>{!! $p->e($item['title']) !!}</title>
-      <link>{!! $p->e($item['link']) !!}</link>
-      <description>{!! $p->e($item['content']) !!}</description>
-      <author>{!! $p->e($item['email']) !!} ({!! $p->e($item['author']) !!})</author>
-      <guid>{!! $p->e($item['id']) !!}</guid>
-      <pubDate>{!! $p->e(\gmdate('r', $item['published'])) !!}</pubDate>
+      <title>{{ $item['title'] }}</title>
+      <link>{{ $item['link'] }}</link>
+      <description>{{ $item['content'] }}</description>
+      <author>{{ $item['email'] }} ({{ $item['author'] }})</author>
+      <guid>{{ $item['id'] }}</guid>
+      <pubDate>{{ \gmdate('r', $item['published']) }}</pubDate>
     </item>
 @endforeach
   </channel>
