8b12b5750e55d5a18429c11f884fd9f25c266664
Visman <visman2008@googlemail.com>
2021-02-16 18:05:19 +0700

    Update Ban\IsBanned
    Now the isBanned() method will return an exception if email is empty.
    The result returns the type of ban: solo ban for email or banned the entire domain.

--- app/Models/BanList/IsBanned.php
+++ app/Models/BanList/IsBanned.php
@@ -12,54 +12,62 @@ namespace ForkBB\Models\BanList;
 
 use ForkBB\Models\Method;
 use ForkBB\Models\User\Model as User;
+use InvalidArgumentException;
 
 class IsBanned extends Method
 {
     /**
      * Проверяет наличие бана пользователя на основании email
+     *
+     * результат: 0 - для этого email нет бана
+     *            1 - забанен именно этот email
+     *            2 - забанен домен
      */
     public function isBanned(User $user): int
     {
-        if (
-            $user->isGuest
-            && ! empty($this->model->emailList)
-            && $user->email_normal
-        ) {
-            $email = $this->model->trimToNull($user->email_normal);
-            $stage = 0;
-
-            do {
-                if (isset($this->model->emailList[$email])) {
-                    return $this->model->emailList[$email];
-                }
-
-                switch ($stage) {                               // "super@user"@example.com
-                    case 0:
-                        $pos = \strrpos($email, '@');
-
-                        if (false !== $pos) {
-                            $email = \substr($email, $pos + 1); // -> example.com
-                            break;
-                        }
-
-                        ++$stage;
-                    case 1:
-                        $email = '.' . $email;                  // -> .example.com
-                        $pos = true;
-                        break;
-                    default:
-                        $pos = \strpos($email, '.', 1);
-
-                        if (false !== $pos) {
-                            $email = \substr($email, $pos);     // -> .com
-                        }
-
-                        break;
-                }
-
-                ++$stage;
-            } while (false !== $pos);
+        if (empty($this->model->emailList)) {
+            return 0;
         }
+
+        $email = $this->model->trimToNull($user->email_normal);
+
+        if (null === $email) {
+            throw new InvalidArgumentException('Expected email, not empty string');
+        }
+
+        $stage = 0;
+
+        do {
+            if (isset($this->model->emailList[$email])) {
+                return false === \strpos($email, '@') ? 2 : 1;
+            }
+
+            switch ($stage) {                               // "super@user"@example.com
+                case 0:
+                    $pos = \strrpos($email, '@');
+
+                    if (false !== $pos) {
+                        $email = \substr($email, $pos + 1); // -> example.com
+                        break;
+                    }
+
+                    ++$stage;
+                case 1:
+                    $email = '.' . $email;                  // -> .example.com
+                    $pos = true;
+                    break;
+                default:
+                    $pos = \strpos($email, '.', 1);
+
+                    if (false !== $pos) {
+                        $email = \substr($email, $pos);     // -> .com
+                    }
+
+                    break;
+            }
+
+            ++$stage;
+        } while (false !== $pos);
 
         return 0;
     }
