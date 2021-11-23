5490c4c0e6740361add306cd71b6cdd86204037d
Visman <visman2008@googlemail.com>
2021-11-24 00:09:49 +0700

    Update style for crumbs template

--- app/templates/layouts/crumbs.forkbb.php
+++ app/templates/layouts/crumbs.forkbb.php
@@ -4,12 +4,12 @@
     @foreach ($p->crumbs as $cur)
         @if (\is_object($cur[0]))
           <li class="f-crumb @if ($cur[0]->is_subscribed) f-subscribed @endif"><!-- inline -->
-            <a href="{{ $cur[0]->link }}" @if ($cur[2]) class="active" @endif>{{ $cur[1] }}</a>
+            <a class="f-crumb-a @if ($cur[2]) active @endif" href="{{ $cur[0]->link }}">{{ $cur[1] }}</a>
           </li><!-- endinline -->
         @else
           <li class="f-crumb"><!-- inline -->
             @if ($cur[0])
-            <a href="{{ $cur[0] }}" @if ($cur[2]) class="active" @endif>{{ $cur[1] }}</a>
+            <a class="f-crumb-a @if ($cur[2]) active @endif" href="{{ $cur[0] }}">{{ $cur[1] }}</a>
             @else
             <span @if ($cur[2]) class="active" @endif>{{ $cur[1] }}</span>
             @endif
