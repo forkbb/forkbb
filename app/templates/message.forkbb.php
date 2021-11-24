9346767356ddf3def29ff837581c272e366cce61
Visman <visman2008@googlemail.com>
2021-11-24 10:44:02 +0700

    Update style for message template

--- app/templates/message.forkbb.php
+++ app/templates/message.forkbb.php
@@ -1,6 +1,6 @@
 @extends ('layouts/main')
 @if ($p->back)
     <div id="fork-bcklnk">
-      <p><a class="f-go-back" href="{{ $p->fRootLink }}">{!! __('Go back') !!}</a></p>
+      <p id="fork-bcklnk-p"><a class="f-go-back" href="{{ $p->fRootLink }}">{!! __('Go back') !!}</a></p>
     </div>
 @endif
