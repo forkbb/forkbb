c9869392ed085b1a56370c666120d2d5037fcfa5
Visman <visman2008@googlemail.com>
2022-01-01 21:30:37 +0700

    Update Post\PreviousPost
    Optimize query.

--- app/Models/Post/PreviousPost.php
+++ app/Models/Post/PreviousPost.php
@@ -24,12 +24,18 @@ class PreviousPost extends Action
             ':pid' => $post->id,
             ':tid' => $post->topic_id,
         ];
-        $field = $returnId ? 'id' : 'posted';
-        $query = "SELECT p.{$field}
-            FROM ::posts AS p
-            WHERE p.id < ?i:pid AND p.topic_id=?i:tid
-            ORDER BY p.id DESC
-            LIMIT 1";
+
+        if ($returnId) {
+            $query = 'SELECT MAX(p.id)
+                FROM ::posts AS p
+                WHERE p.id < ?i:pid AND p.topic_id=?i:tid';
+        } else {
+            $query = 'SELECT p.posted
+                FROM ::posts AS p
+                WHERE p.id < ?i:pid AND p.topic_id=?i:tid
+                ORDER BY p.id DESC
+                LIMIT 1';
+        }
 
         $id = (int) $this->c->DB->query($query, $vars)->fetchColumn();
 
