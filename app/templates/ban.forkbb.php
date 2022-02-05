d412cf465beab765432f51d497c541a695fa63fc
Visman <visman2008@googlemail.com>
2022-02-05 14:02:26 +0700

    Update ban.forkbb.php template

--- app/templates/ban.forkbb.php
+++ app/templates/ban.forkbb.php
@@ -2,16 +2,16 @@
     <section id="fork-ban" class="f-main">
       <h2>{!! __('Info') !!}</h2>
 @if ($p->bannedIp)
-      <p>{!! __('Ban message 1') !!}</p>
+      <p>{!! __('Your IP is blocked') !!}</p>
 @else
-      <p>{!! __('Ban message') !!}</p>
+      <p>{!! __('You are banned') !!}</p>
 @endif
 @if ($p->ban['expire'])
-      <p>{!! __(['Ban message 2', dt($p->ban['expire'], true)]) !!}</p>
+      <p>{!! __(['The ban expires %s', dt($p->ban['expire'], true)]) !!}</p>
 @endif
 @if ($p->ban['message'])
-      <p>{!! __('Ban message 3') !!}</p>
+      <p>{!! __('Ban message for you') !!}</p>
       <p><b>{{ $p->ban['message'] }}</b></p>
 @endif
-      <p>{!! __(['Ban message 4', $p->adminEmail]) !!}</p>
+      <p>{!! __(['Ban message contact %s', $p->adminEmail]) !!}</p>
     </section>
