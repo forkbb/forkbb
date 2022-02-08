7b77ef3c2f9f46ff76f03d756cd02d5a441d5deb
Visman <visman2008@googlemail.com>
2022-02-08 14:18:09 +0700

    Update Rules page

--- app/Models/Pages/Rules.php
+++ app/Models/Pages/Rules.php
@@ -20,17 +20,18 @@ class Rules extends Page
      */
     public function view(): Page
     {
-        $this->fIndex     = self::FI_RULES;
-        $this->nameTpl    = 'rules';
-        $this->onlinePos  = 'rules';
-        $this->canonical  = $this->c->Router->link('Rules');
-        $this->crumbs     = $this->crumbs(
+        $this->fIndex       = self::FI_RULES;
+        $this->nameTpl      = 'rules';
+        $this->onlinePos    = 'rules';
+        $this->onlineDetail = null;
+        $this->canonical    = $this->c->Router->link('Rules');
+        $this->crumbs       = $this->crumbs(
             [
                 $this->c->Router->link('Rules'),
                 'Forum rules',
             ]
         );
-        $this->rules      = $this->c->config->o_rules_message;
+        $this->rules        = $this->c->config->o_rules_message;
 
         return $this;
     }
@@ -42,19 +43,20 @@ class Rules extends Page
     {
         $this->c->Lang->load('register');
 
-        $this->fIndex     = self::FI_REG;
-        $this->nameTpl    = 'rules';
-        $this->onlinePos  = 'rules';
-        $this->robots     = 'noindex';
-        $this->crumbs     = $this->crumbs(
+        $this->fIndex       = self::FI_REG;
+        $this->nameTpl      = 'rules';
+        $this->onlinePos    = 'rules';
+        $this->onlineDetail = null;
+        $this->robots       = 'noindex';
+        $this->crumbs       = $this->crumbs(
             'Forum rules',
             [
                 $this->c->Router->link('Register'),
                 'Register',
             ]
         );
-        $this->rules      = 1 === $this->c->config->b_rules ? $this->c->config->o_rules_message : __('If no rules');
-        $this->form       = $this->formAgree();
+        $this->rules        = 1 === $this->c->config->b_rules ? $this->c->config->o_rules_message : __('If no rules');
+        $this->form         = $this->formAgree();
 
         return $this;
     }
