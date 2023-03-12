230e56b71617ec762387b3093edb9b1768eece7c
Visman <visman2008@googlemail.com>
2023-03-12 22:48:41 +0700

    Update defaultBBCode.php
    For quote bbcode.

--- app/config/defaultBBCode.php
+++ app/config/defaultBBCode.php
@@ -290,7 +290,7 @@ HANDLER,
             'No_attr' => true,
         ],
         'handler' => <<<'HANDLER'
-$header = isset($attrs['Def']) ? '<div class="f-bb-q-header">' . $attrs['Def'] .  ' ' . __('wrote') . '</div>' : '';
+$header = isset($attrs['Def']) ? '<div class="f-bb-q-header">' . __(['%s wrote', $attrs['Def']]) . '</div>' : '';
 return "</p><blockquote class=\"f-bb-quote\">{$header}<div class=\"f-bb-q-body\"><p>{$body}</p></div></blockquote><p>";
 HANDLER,
     ],
