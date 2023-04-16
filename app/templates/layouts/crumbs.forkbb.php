e6c658a23f6a8fe2488c9db7d690254307641172
Visman <visman2008@googlemail.com>
2023-04-16 23:06:49 +0700

    Add aria-current="page"
    I'm not sure if this is done correctly. If there are several pages in pagination?

--- app/templates/layouts/crumbs.forkbb.php
+++ app/templates/layouts/crumbs.forkbb.php
@@ -4,7 +4,7 @@
     @foreach ($p->crumbs as $cur)
         @if (\is_object($cur[0]))
           <li class="f-crumb @if ($cur[0]->is_subscribed) f-subscribed @endif" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><!-- inline -->
-            <a class="f-crumb-a @if ($cur[2]) active @endif" href="{{ $cur[0]->link }}" itemprop="item">
+            <a class="f-crumb-a @if ($cur[2]) active" aria-current="page @endif" href="{{ $cur[0]->link }}" itemprop="item">
               <span itemprop="name">{!! __($cur[1]) !!}</span>
             </a>
             <meta itemprop="position" content="{!! @iteration !!}">
@@ -12,7 +12,7 @@
         @else
           <li class="f-crumb" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><!-- inline -->
             @if ($cur[0])
-            <a class="f-crumb-a @if ($cur[2]) active @endif" href="{{ $cur[0] }}" itemprop="item">
+            <a class="f-crumb-a @if ($cur[2]) active" aria-current="page @endif" href="{{ $cur[0] }}" itemprop="item">
               <span itemprop="name">{!! __($cur[1]) !!}</span>
             </a>
             @else
