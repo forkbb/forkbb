6e0923ac448c68b7add7847c964e2810b9b82e8e
Visman <visman2008@googlemail.com>
2023-07-15 12:41:48 +0700

    Update topic/topic_in_search templates

--- app/templates/topic_in_search.forkbb.php
+++ app/templates/topic_in_search.forkbb.php
@@ -47,6 +47,9 @@
             <span class="f-sep"><small>»</small></span>
             <span class="f-post-posted"><a href="{{ $post->link }}" title="{{ __('Go to post') }}" rel="bookmark"><time datetime="{{ \gmdate('c', $post->posted) }}">{{ dt($post->posted) }}</time></a></span>
           </h3>
+        @if ($post->edited)
+          <span class="f-post-edited" title="{{ __(['Last edit', $post->editor, dt($post->edited)]) }}"><span>{!! __('Edited') !!}</span></span>
+        @endif
           <span class="f-post-number">#{{ $post->postNumber }}</span>
         </header>
         <address class="f-post-user">
