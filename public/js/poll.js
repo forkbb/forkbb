65c7b5d7acdfee703d16017eab8cc42e4d054317
Visman <visman2008@googlemail.com>
2023-07-17 00:20:55 +0700

    Update poll.js

--- public/js/poll.js
+++ public/js/poll.js
@@ -1,3 +1,10 @@
+/**
+ * This file is part of the ForkBB <https://github.com/forkbb>.
+ *
+ * @copyright (c) Visman <mio.visman@yandex.ru, https://github.com/MioVisman>
+ * @license   The MIT License (MIT)
+ */
+
 if (typeof ForkBB === "undefined" || !ForkBB) {
     var ForkBB = {};
 }
