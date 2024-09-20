99d86e609360449a50103f8c5224dc84c49ec91a
Visman <visman2008@googlemail.com>
2024-09-20 09:42:47 +0700

    Add bot tag just in case

--- app/Models/Pages/Register.php
+++ app/Models/Pages/Register.php
@@ -272,6 +272,7 @@ class Register extends Page
         $user->ip_check_type   = 0;
         $user->location        = $this->useOAuth ? $this->provider->userLocation : '';
         $user->url             = $this->useOAuth ? $this->provider->userURL : '';
+        $user->admin_note      = false === $this->user->isBot ? '' : '{bot reg}';
 
         if (
             $this->useOAuth
