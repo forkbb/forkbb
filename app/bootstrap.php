d4b5db44d2f8c365026db18f1feb1dd8da857fd4
Visman <visman2008@googlemail.com>
2024-12-19 19:40:13 +0700

    Don't rush to update!

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -72,7 +72,7 @@ if (
     $c->BASE_URL = \str_replace('https://', 'http://', $c->BASE_URL);
 }
 
-$c->FORK_REVISION = 84;
+$c->FORK_REVISION = 85;
 $c->START         = $forkStart;
 $c->PUBLIC_URL    = $c->BASE_URL . $forkPublicPrefix;
 
