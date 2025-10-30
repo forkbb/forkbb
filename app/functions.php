2b9d0862ef38bfec59410ec163ae5f96ea258b03
Visman <visman2008@googlemail.com>
2025-10-30 22:10:27 +0700

    Rev up

--- app/functions.php
+++ app/functions.php
@@ -19,7 +19,7 @@ use InvalidArgumentException;
 \ini_set('log_errors', '1');
 
 define('FORK_START', $_SERVER['REQUEST_TIME_FLOAT'] ?? \microtime(true));
-define('FORK_REVISION', 89);
+define('FORK_REVISION', 90);
 
 \setlocale(\LC_ALL, 'C');
 \mb_language('uni');
