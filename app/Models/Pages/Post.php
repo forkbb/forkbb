d9f79cd11d690d2180faf50111e4a6ba00e1a5cd
Visman <visman2008@googlemail.com>
2026-04-15 22:12:28 +0700

    Pages\Post: Add addNotifications() method (sketch)

--- app/Models/Pages/Post.php
+++ app/Models/Pages/Post.php
@@ -15,6 +15,7 @@ use ForkBB\Models\Model;
 use ForkBB\Models\Page;
 use ForkBB\Models\Draft\Draft;
 use ForkBB\Models\Forum\Forum;
+use ForkBB\Models\Post\Post;
 use ForkBB\Models\Topic\Topic;
 use function \ForkBB\__;
 
@@ -518,6 +519,30 @@ class Post extends Page
             }
         }
 
+        if (1 === $this->config->b_notifications) {
+            $this->addNotifications($v->message, $merge ? $lastPost : $post);
+        }
+
         return $this->c->Redirect->url(/*$merge ? $lastPost->link : $post->link*/ $topic->linkLast)->message('Post redirect', FORK_MESS_SUCC);
+    }
+
+    protected function addNotifications(string $text, Post $post): void
+    {
+        list($nQuoted, $nContact) = $this->c->Parser->findNicknames($text);
+        $nicks                    = \array_merge($nQuoted, $nContact);
+
+        if (empty($nicks)) {
+            return;
+        }
+
+        foreach ($nicks as $nick => $z) {
+            $user = $this->c->users->loadByName($nick, true);
+
+            if (null === $user) {
+                continue;
+            }
+
+            $this->c->notifications->addNicknameMention($user, $post, ['quoted' => isset($nQuoted[$nick]), 'contact' => isset($nContact[$nick])]);
+        }
     }
 }
